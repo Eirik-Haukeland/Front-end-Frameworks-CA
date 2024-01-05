99dd7b3b07c5e9c49ea1b44226f6896df7e7409c
Eirik Berget Haukeland <eirik@haukeland.name>
2024-01-05 14:34:38 +0100

    add buttons to cart

--- src/compontents/Button/Button.jsx
+++ src/compontents/Button/Button.jsx
@@ -1,7 +1,17 @@
 import buttonCss from "./Button.module.css"
 
-export default function Button ({children}) {
+export default function Button ({children, onEventFunction, className, disabled}) {
+
+  const isTabIndex = typeof onEventFunction === 'function' && disabled === false ? {tabIndex: 0} : {}
+  const isClassName = typeof className !== 'undefined'
+
   return (
-    <span className={buttonCss.button} >{children}</span>
+    <span 
+      onClick={onEventFunction} 
+      onKeyDown={(e) => e.code === "Enter" && onEventFunction()}
+      {...isTabIndex}
+      className={`${buttonCss.button} ${isClassName && className}`} 
+      disabled={disabled}
+    >{children}</span>
   )
 }
--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,4 +1,3 @@
-import logo from "../../logo.svg";
 import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import {getSingleProduct} from "../../util/api.js";
@@ -6,15 +5,19 @@ import DisplayPrice from "../../compontents/DisplayPrice/DisplayPrice.jsx"
 import productCss from "./Product.module.css";
 import Button from "../../compontents/Button/Button.jsx";
 import RevueCard from "../../compontents/RevueCard/RevueCard.jsx";
+import { addItemsToCart, isItemInCart } from "../../util/cartFuncs.js";
 
 export default function Product () {
 	const {productId} = useParams()
 
 	let [product, setProduct] = useState({})
+	let [isInCart, setIsInCart] = useState(false)
 	useEffect(() => {
 		getSingleProduct(productId).then(setProduct)
+		setIsInCart(isItemInCart(productId))
 	}, []);
 	
+
 	return (
 		<>
 			<seciton className={productCss.productSection}>
@@ -26,7 +29,7 @@ export default function Product () {
 					</div>
 					<div className={productCss.price_and_btn}>
 						<DisplayPrice price={product.price} discountedPrice={product.discountedPrice} />
-						<Button>Add To Cart</Button>
+						<Button onEventFunction={() => {addItemsToCart(product.id); setIsInCart(isItemInCart(product.id));}} disabled={isInCart}>Add To Cart</Button>
 					</div>
 				</div>
 			</seciton>
