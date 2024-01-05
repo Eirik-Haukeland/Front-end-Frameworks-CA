31c8a2ec618a06a45cad10606507ea0618dda449
Eirik Berget Haukeland <eirik@haukeland.name>
2024-01-05 20:49:49 +0100

    added cart styling file, listing out items in cart

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,23 +1,38 @@
-import logo from "../../logo.svg";
+import { useEffect, useState } from "react"
+
+import { getAllItemsFromCart } from "../../util/cartFuncs"
+import cartCss from "./Cart.module.css"
+import { getListedProducts } from "../../util/api"
+import Button from "../../compontents/Button/Button"
+import DisplayPrice from "../../compontents/DisplayPrice/DisplayPrice"
+
 
 export default function Cart () {
+  const [itemIds, setItemIds] = useState([])
+  useEffect(() => {  
+    setItemIds(getAllItemsFromCart())
+  }, [])
+  
+  const [itemsInCart, setItemsInCart] = useState()
+  useEffect(() => {
+    if (itemIds.length >= 1) {
+      getListedProducts(itemIds).then(setItemsInCart)
+    }
+  }, [itemIds])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-         Cartman
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="cartCss.cartSection">
+      <h1>Shopping Cart</h1>
+      {
+        itemsInCart?.map(item => 
+          <div key={item.id}>
+            <img src={item.imageUrl} alt=""/>
+            <h2>item.title</h2>
+            <Button>remove</Button>
+            <DisplayPrice price={item.price} discountedPrice={item.discountedPrice} />
+          </div>)
+      }
+    </section>
   )
 }
 
