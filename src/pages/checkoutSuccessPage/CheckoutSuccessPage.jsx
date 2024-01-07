0cf3a686154f6c787cd1be2b3805a9c30042aff8
Eirik Berget Haukeland <eirik@haukeland.name>
2024-01-07 10:27:41 +0100

    started fixing checkout page

--- src/pages/checkoutSuccessPage/CheckoutSuccessPage.jsx
+++ src/pages/checkoutSuccessPage/CheckoutSuccessPage.jsx
@@ -1,22 +1,33 @@
-import logo from "../../logo.svg";
+import { useState, useEffect } from "react";
+import Button from "../../compontents/Button/Button"
+import ProductCard from "../../compontents/ProductCard/ProductCard"
+import { getListedProducts } from "../../util/api";
+import { getAllItemsFromCart } from "../../util/cartFuncs";
+import { Link } from "react-router-dom";
+import successCss from "./CheckoutSuccess.module.css"
 
 export default function CheckoutSuccessPage () {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    getListedProducts(getAllItemsFromCart()).then(setProducts)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-         Cartman
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={successCss.containter}>
+      <div className={successCss.thankYouAria}>
+        <h1>Purhase Complete</h1>
+        <span>Thank you For Your Purhase</span>
+        <Link to="/" style={{textDecoration: "none"}}>
+          <Button>Back to Store</Button>
+        </Link>
+      </div>
+      <section className={successCss.purchaseSection}>
+        <h2 className="sr-only">Your Purchase:</h2>
+        {
+          products.map(product => ProductCard(product))
+        }
+      </section>
     </div>
   )
 }
