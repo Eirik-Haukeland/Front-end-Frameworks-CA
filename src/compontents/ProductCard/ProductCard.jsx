e609cbb716ce8465ba9a571bd6c8c5d1036f8e80
Eirik Berget Haukeland <eirik@haukeland.name>
2024-01-07 22:59:21 +0100

    added missing slash

--- src/compontents/ProductCard/ProductCard.jsx
+++ src/compontents/ProductCard/ProductCard.jsx
@@ -6,7 +6,7 @@ export default function ProductCard (props) {
   const {id, title, imageUrl, price, discountedPrice} = props
 
   return (
-    <Link className={cardCss.card_container} key={id} to={`product/${id}`}>
+    <Link className={cardCss.card_container} key={id} to={`/product/${id}`}>
       <img className={cardCss.image} src={imageUrl} alt="" />
       <span className={cardCss.title}>{title}</span>
       {discountedPrice !== price
